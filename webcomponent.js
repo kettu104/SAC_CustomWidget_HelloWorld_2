0bfc7b3bd912559080e5f8d6163b46a029466969
kettu104 <kettu104@gmail.com>
2021-06-15 20:50:00 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -10,14 +10,16 @@
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
             this._tagContainer;
             this._tagType = "h1";
-            this._tagText = "";
+            this._tagText = "Hello World";
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this.redraw();
+            this._firstConnection = true;
+            this.redraw();           
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -32,7 +34,9 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
